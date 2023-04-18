a5b471060ba0997d3b1ac98f986d2045330c9818
ANPA1234 <anjaneyulude159@gmail.com>
2023-04-18 09:18:28 -0400

    Added java project2023

---
+++ src/main/java/com/careerit/java/day1/helloworld.java
@@ -0,0 +1,10 @@
+package com.careerit.java.day1;
+
+import java.sql.SQLOutput;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("welcome to java world");
+
+    }
+}
