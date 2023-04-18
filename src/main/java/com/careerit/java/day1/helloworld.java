804d3e705b0afca80162b3c69df0cce7cf68da9e
ANPA1234 <anjaneyulude159@gmail.com>
2023-04-18 09:25:35 -0400

    Added java project2023

--- src/main/java/com/careerit/java/day1/helloworld.java
+++ src/main/java/com/careerit/java/day1/helloworld.java
@@ -5,6 +5,9 @@ import java.sql.SQLOutput;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("welcome to java world");
+        for(int i=1;i<=5;i++){
+            System.out.println("welcome user :"+i);
+        }
 
     }
 }
